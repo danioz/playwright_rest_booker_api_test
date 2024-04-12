f24d9e5264803e6f335f1754980c81a4472d62e4
DanioZ <daniozet@gmail.com>
2024-04-12 21:57:55 +0200

    add describe

--- tests/booking. get.spec.ts
+++ tests/booking. get.spec.ts
@@ -5,37 +5,47 @@ import { RequestHeaders, createHeaders, createInvalidHeaders } from '@helpers/he
 let validHeaders: RequestHeaders;
 let invalidHeaders: RequestHeaders;
 
-test.beforeAll(async () => {
-  validHeaders = await createHeaders();
-  invalidHeaders = await createInvalidHeaders();
-});
+test.describe('GET /booking', () => {
+  test.beforeAll(async () => {
+    validHeaders = await createHeaders();
+    invalidHeaders = await createInvalidHeaders();
+  });
 
-test('GET all bookings', async ({ request }) => {
-  //Act
-  const response = await request.get('/booking');
+  test('GET all bookings', async ({ request }) => {
+    //Act
+    const response = await request.get('/booking');
 
-  //Assert
-  expect(response.status()).toBe(200);
+    //Assert
+    expect(response.status()).toBe(200);
 
-  const body: Booking[] = await response.json();
-  expect(body.length).toBeGreaterThan(0);
-});
+    const body: Booking[] = await response.json();
+    expect(body.length).toBeGreaterThan(0);
+  });
 
-test('GET booking for specific booking based upon the booking id provided', async ({ request }) => {
-  //Act
-  const response = await request.get('booking/1');
+  test('GET booking for specific booking based upon the booking id provided', async ({ request }) => {
+    //Act
+    const response = await request.get('booking/1');
 
-  //Assert
-  expect(response.status()).toBe(200);
+    //Assert
+    expect(response.status()).toBe(200);
 
-  const body: Booking = await response.json();
-  expect(Date.parse(body.bookingdates.checkin)).toBeLessThan(Date.parse(body.bookingdates.checkout));
-});
+    const body: Booking = await response.json();
+    expect(Date.parse(body.bookingdates.checkin)).toBeLessThan(Date.parse(body.bookingdates.checkout));
+  });
 
-test('GET booking summary with non existing room', async ({ request }) => {
-  //Act
-  const response = await request.get('booking/99999');
-  
-  //Assert
-  expect(response.status()).toBe(404);
+  test('GET booking with non existing room', async ({ request }) => {
+    //Act
+    const response = await request.get('booking/99999');
+
+    //Assert
+    expect(response.status()).toBe(404);
+  });
+
+  test('GET booking ???', async ({ request }) => {
+    //Act
+    const response = await request.get('booking/99999');
+
+    //Assert
+    expect(response.status()).toBe(404);
+  });
 });
