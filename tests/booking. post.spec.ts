7639e91b3e2a05782e4acfe3107c005950e7a304
DanioZ <daniozet@gmail.com>
2024-04-30 10:06:11 +0200

    Add tests for POST /booking endpoint

---
+++ tests/booking. post.spec.ts
@@ -0,0 +1,47 @@
+import { test, expect } from '@playwright/test';
+import { Booking } from '@helpers/booking/booking-model';
+import { BookingRequests } from '@helpers/booking/booking-requests';
+import { RequestHeaders, createHeaders, createInvalidHeaders } from '@helpers/headers';
+import { DataFactory } from '@helpers/data/data-factory';
+
+let validHeaders: RequestHeaders;
+let invalidHeaders: RequestHeaders;
+let bookingRequests: BookingRequests;
+
+test.describe('POST /booking', () => {
+  test.beforeAll(async () => {
+    validHeaders = await createHeaders();
+    invalidHeaders = await createInvalidHeaders();
+  });
+
+  test.beforeEach(async () => {
+    bookingRequests = new BookingRequests();
+  });
+
+  test('POST new booking', async () => {
+    //Arrange
+    const bookingData: Booking = DataFactory.getBooking();
+    //Act
+    const response = await bookingRequests.createBooking(bookingData);
+    //Assert
+    expect(response.response.status()).toBe(200);
+
+    const body = response.responseBody;
+    expect(body.id).not.toBeNull();
+
+    const booking: Booking = body.booking;
+    expect(booking).toMatchObject(bookingData);
+  });
+
+  test('POST new booking with random data', async () => {
+    //Act
+    const response = await bookingRequests.createBooking();
+    //Assert
+    expect(response.response.status()).toBe(200);
+
+    const body = response.responseBody;
+    expect(body.id).not.toBeNull();
+  });
+
+  test('BOOKING new', async () => {});
+});
