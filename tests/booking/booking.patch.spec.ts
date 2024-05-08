25ef2d62e5cbad4feda6ac66f363b94ba2bf9715
DanioZ <daniozet@gmail.com>
2024-05-08 22:59:10 +0200

    chore: Add PATCH test for updating booking details

--- tests/booking/booking.patch.spec.ts
+++ tests/booking/booking.patch.spec.ts
@@ -33,4 +33,44 @@ test.describe('PATCH /booking', () => {
       });
     }
   );
+
+  const patches = [
+    {
+      firstname: 'John',
+      lastname: 'Doe',
+    },
+    {
+      totalprice: 999,
+    },
+    {
+      depositpaid: false,
+    },
+  ];
+  for (const patch of patches) {
+    test(
+      `PATCH booking with body ${JSON.stringify(patch)}`,
+      { tag: Tag.SMOKE_TEST },
+      async ({ bookingRequests, bookingData, validHeaders, createdBookingId }) => {
+        const createBooking = await bookingRequests.createBooking(bookingData);
+        const createdBookingBody = createBooking.responseBody.booking;
+
+        const patchedBookingBody = { ...createdBookingBody, ...patch };
+
+        //Act
+        const res = await bookingRequests.patchBooking(createdBookingId, patch, validHeaders);
+
+        //Assert
+        expect(res.response.status()).toBe(200);
+
+        const booking: Booking = res.responseBody;
+        expect(booking).toMatchObject(patchedBookingBody);
+
+        await test.step('Verify booking was updated', async () => {
+          const updatedBooking = await bookingRequests.getBookingById(createdBookingId);
+          expect(updatedBooking.response.status()).toBe(200);
+          expect(updatedBooking.responseBody).toMatchObject(patchedBookingBody);
+        });
+      }
+    );
+  }
 });
