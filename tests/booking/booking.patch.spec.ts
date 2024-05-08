3b04d9c93eb67b309ff703cf54302a24126a2f8c
DanioZ <daniozet@gmail.com>
2024-05-08 11:43:09 +0200

    chore: Add initial test for PATCH /booking endpoint

---
+++ tests/booking/booking.patch.spec.ts
@@ -0,0 +1,7 @@
+import { test, expect } from '../fixtures';
+import { Booking } from '@helpers/booking/booking-model';
+import Tag from 'lib/tag';
+
+test.describe('PATCH /booking', () => {
+  
+});
